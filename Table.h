1f728fad67700e6f508f92730fcaa80a4a8a0f6c
JustinFarinaccio <fari0040@algonquinlive.com>
2015-10-27 15:52:29 -0400

    Update Table.h

--- Table.h
+++ Table.h
@@ -1,17 +1,25 @@
-/* Filename: table.h
-* Transition Table and function declarations necessary for the scanner implementation  
-* as required for CST8152 - Assignment #2.
-* Version: 1.15.02
-* Date: 29 September 2015
-* Provided by: Svillen Ranev
-* The file is incomplete. You are to complete it.
-***************************************************
-* REPLACE THIS HEADER WITH YOUR HEADER
-***************************************************
-*/
+/*****************************************************************************************************
+ * File Name:		table.h
+ * Compiler:		MS Visual Studio 2012
+ * Authors:			Bryden Lacelle & Justin Farinaccio
+ * Course:			CST8152 - Compilers, Lab Section 011
+ * Assignment:		Assignment 2: The Scanner
+ * Date:			October 27, 2015
+ * Professor:		Svillen Ranev
+ * Purpose:			Header file for scanner.c; includes definitions for error/invalid and accepting states, 
+					the transition table, accepting state table, and list of keywords, as well as other 
+					necessary definitions and typedefs.
+ * Function List:	aa_func02();
+					aa_func03();
+					aa_func05();
+					aa_func08();
+					aa_func10();
+					aa_func12();
+					aa_func13()
+*****************************************************************************************************/
 
 #ifndef  TABLE_H_
-#define  TABLE_H_ 
+#define  TABLE_H_
 
 #ifndef BUFFER_H_
 #include "buffer.h"
@@ -21,106 +29,68 @@
 #include <_null.h> /* NULL pointer constant is defined there */
 #endif
 
-/*   Source end-of-file (SEOF) sentinel symbol
-*    '\0' or only one of the folowing constants: 255, 0xFF , EOF
-*/
-
-/*  Single-lexeme tokens processed separately one by one
-*  in the token-driven part of the scanner
-*  '=' , ' ' , '(' , ')' , '{' , '}' , == , <> , '>' , '<' ,
-*       space
-*  !<comment , ',' , '"' , ';' , '-' , '+' , '*' , '/', # ,
-*  .AND., .OR. , SEOF, 'wrong symbol',
-*/
-
-
-/* REPLACE *ESN* WITH YOUR ERROR STATE NUMBER */
-#define ES -2	/* Error state */
+/* define error and invalid states */
+#define ES 12	/* Error state */
 #define IS -1	/* Invalid state */
 
-	/* State transition table definition */
-
-/*	REPLACE *CN* WITH YOUR COLUMN NUMBER  */
-
+/* State transition table definition */
 #define TABLE_COLUMNS 7
-	/*transition table - type of states defined in separate table */
-	int st_table[ ][TABLE_COLUMNS] = {
-		/* State 0 */	{1, 6, 4, 4, IS, IS, IS}, 
-		/* State 1 */	{1, 1, 1, 1, ES, 3, 2}, 
-		/* State 2 */	{IS, IS, IS, IS, IS, IS, IS}, 
-		/* State 3 */	{IS, IS, IS, IS, IS, IS, IS}, 
-		/* State 4 */	{ES, 4, 4, 4, 7, 5, 5}, 
-		/* State 5 */	{IS, IS, IS, IS, IS, IS, IS}, 
-		/* State 6 */	{ES, ES, 9, ES, 7, ES, 5}, 
-		/* State 7 */	{ES, 7, 7, 7, 8, 8, 8}, 
-		/* State 8 */	{IS, IS, IS, IS, IS, IS, IS}, 
-		/* State 9 */	{ES, 9, 9, IS, ES, 10, 10}, 
-		/* State 10 */	{IS, IS, IS, IS, IS, IS, IS}, 
-		/* State 11 */	{IS, IS, IS, IS, IS, IS, IS}, 
-		/* State 12 */	{IS, IS, IS, IS, IS, IS, IS}, 
-		/* State 13 */	{IS, IS, IS, IS, IS, IS, IS}
-	};
-		/* Accepting state table definition */
-/*		REPLACE *N1*, *N2*, and *N3* WITH YOUR NUMBERS */
-#define ASWR 97		/* accepting state with retract */
-#define ASNR 98		/* accepting state with no retract */
-#define NOAS 99		/* not accepting state */
 
-		int as_table[ ] = {NOAS, NOAS, ASWR, ASNR, NOAS, ASWR, NOAS, NOAS, ASWR, NOAS, ASWR, ASNR, ASWR};
+/* Transition table - type of states defined in separate table */
+int st_table[ ][TABLE_COLUMNS] = {
+	/* State 0 */	{1, 6, 4, 4, IS, IS, IS}, 
+	/* State 1 */	{1, 1, 1, 1, 2, 3, 2}, 
+	/* State 2 */	{IS, IS, IS, IS, IS, IS, IS}, 
+	/* State 3 */	{IS, IS, IS, IS, IS, IS, IS}, 
+	/* State 4 */	{ES, 4, 4, 4, 7, 5, 5}, 
+	/* State 5 */	{IS, IS, IS, IS, IS, IS, IS}, 
+	/* State 6 */	{ES, ES, 9, ES, 7, ES, 5}, 
+	/* State 7 */	{8, 7, 7, 7, 8, 8, 8}, 
+	/* State 8 */	{IS, IS, IS, IS, IS, IS, IS}, 
+	/* State 9 */	{ES, 9, 9, ES, ES, 10, 10}, 
+	/* State 10 */	{IS, IS, IS, IS, IS, IS, IS}, 
+	/* State 11 */	{IS, IS, IS, IS, IS, IS, IS}, 
+	/* State 12 */	{IS, IS, IS, IS, IS, IS, IS}, 
+	/* State 13 */	{IS, IS, IS, IS, IS, IS, IS}
+};
 
-		/* Accepting action function declarations */
+/* Accepting state definitions */
+#define ASWR 97	/* accepting state with retract */
+#define ASNR 98	/* accepting state with no retract */
+#define NOAS 99	/* not accepting state */
 
-/*		FOR EACH OF YOUR ACCEPTING STATES YOU MUST PROVIDE
-			ONE FUNCTION PROTOTYPE. THEY ALL RETURN Token AND TAKE
-			ONE ARGUMENT: A string REPRESENTING A TOKEN LEXEME. */
+/* Accepting state table */
+int as_table[ ] = {NOAS, NOAS, ASWR, ASNR, NOAS, ASWR, NOAS, NOAS, ASWR, NOAS, ASWR, ASNR, ASWR};
 
-			Token aa_func02(char *lexeme);
-			Token aa_func03(char *lexeme);
-			Token aa_func05(char *lexeme);
-			Token aa_func08(char *lexeme);
-			Token aa_func10(char *lexeme);
-			Token aa_func12(char *lexeme);
-			Token aa_func13(char *lexeme);
+/* Accepting action function declarations */
+Token aa_func02(char*);
+Token aa_func03(char*);
+Token aa_func05(char*);
+Token aa_func08(char*);
+Token aa_func10(char*);
+Token aa_func12(char*);
+Token aa_func13(char*);
 
-/*		Replace XX with the number of the accepting state: 02, 03 and so on. */
+/* define new type: pointer to function (of one char * argument) returning Token */
+typedef Token (*PTR_AAF)(char *lexeme);
 
-			/* defining a new type: pointer to function (of one char * argument) 
-			returning Token
-			*/  
-
-			typedef Token (*PTR_AAF)(char *lexeme);
-
-
-		/* Accepting function (action) callback table (array) definition */
-		/* If you do not want to use the typedef, the equivalent declaration is:
-		* Token (*aa_table[])(char lexeme[]) = {
-		*/
-
-		PTR_AAF aa_table[ ] = {
-
-			NULL, NULL, *aa_func02, *aa_func03, NULL, *aa_func05, NULL, NULL, *aa_func08, NULL, *aa_func10, NULL, *aa_func12, *aa_func12 /* change these last 2 to either 12 OR 13 */
-
-			/*HERE YOU MUST PROVIDE AN INITIALIZATION FOR AN ARRAY OF POINTERS
-			TO ACCEPTING FUNCTIONS. THE ARRAY HAS THE SAME SIZE AS as_table[ ].
-			YOU MUST INITIALIZE THE ARRAY ELEMENTS WITH THE CORRESPONDING
-			ACCEPTING FUNCTIONS (FOR THE STATES MARKED AS ACCEPTING IN as_table[]).
-			THE REST OF THE ELEMENTS MUST BE SET TO NULL.*/
-
-		};
-
-		/* Keyword lookup table (.AND. and .OR. are not keywords) */
+/* Accepting function callback table definition */
+PTR_AAF aa_table[ ] = {
+	NULL, NULL, *aa_func02, *aa_func03, NULL, *aa_func05, NULL, NULL, *aa_func08, NULL, *aa_func10, NULL, *aa_func12, *aa_func12
+};
 
 #define KWT_SIZE 8
 
-		char *kw_table[] = {
-			"ELSE",
-			"IF",
-			"INPUT",
-			"OUTPUT",
-			"PLATYPUS",
-			"REPEAT",
-			"THEN",
-			"USING"   
-		};
+/* Keyword lookup table (.AND. and .OR. are not keywords) */
+char *kw_table[] = {
+	"ELSE",
+	"IF",
+	"INPUT",
+	"OUTPUT",
+	"PLATYPUS",
+	"REPEAT",
+	"THEN",
+	"USING"   
+};
 
 #endif
