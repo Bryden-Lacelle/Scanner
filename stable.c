c942b303f61634aeeaee2099bc14c732d93bb4cd
Bryden-Lacelle <lace0086@algonquinlive.com>
2015-11-14 16:48:15 -0500

    Update stable.c

--- stable.c
+++ stable.c
@@ -1,3 +1,6 @@
+/* ASK SVILLEN ABOUT CHECKING FOR INVALID STD. IS CHECKING FOR SIZE = 0 OR CHEKING FOR NULL BUFFER? 
+	Does everytime the symbol table get updated does it copy that change to the global symbol table?*/
+
 /*******************************************************************************
 * File Name:		scanner.c
 * Compiler:			MS Visual Studio 2012
@@ -11,6 +14,15 @@
 *******************************************************************************/
 
 #define _CRT_SECURE_NO_WARNINGS
+#define ISSTRING	6				//0000000000000110
+#define STRINGFLAG	6				//0000000000000110
+#define ISINT		4				//0000000000000100
+#define INTFLAG		4				//0000000000000100
+#define ISFLOAT		2				//0000000000000010
+#define FLOATFLAG	2				//0000000000000010
+#define UPDATEFLAG	1				//0000000000000001
+#define UFLOATFLAG	65531			//1111111111111011
+#define UINTFLAG	65533			//1111111111111101
 
 /* project header files */
 #include "buffer.h"
@@ -18,6 +30,7 @@
 #include "table.h"
 #include "stable.h"
 
+STD sym_table;
 /*******************************************************************************
 Purpose:			
 Author:				
@@ -43,7 +56,11 @@ Return Value:
 Algorithm:			
 *******************************************************************************/
 int st_install(STD sym_table, char *lexeme, char type, int line) {
-
+	if (st_lookup(sym_table, lexeme) == R_FAIL_1)
+	{ /*TO-DO*/ }
+	//sym_table.st_offset = sym_table.plsBD->getc_offset;
+	sym_table.pstvr->plex = lexeme;
+	sym_table.pstvr->o_line = line;
 
 
 }
@@ -73,8 +90,15 @@ Return Value:
 Algorithm:			
 *******************************************************************************/
 int st_update_type(STD sym_table, int vid_offset, char v_type) {
-
-
+	if (sym_table.pstvr[vid_offset].status_field & STRINGFLAG == ISSTRING)
+		return R_FAIL_1;
+	if ((sym_table.pstvr[vid_offset].status_field & UPDATEFLAG) == 1) 
+		return R_FAIL_1;
+	if (v_type = 'F')
+		sym_table.pstvr[vid_offset].status_field |= UFLOATFLAG;
+	if (v_type = 'I')
+		sym_table.pstvr[vid_offset].status_field |= UINTFLAG;
+	return vid_offset;
 
 }
 
@@ -88,9 +112,10 @@ Return Value:
 Algorithm:			
 *******************************************************************************/
 int st_update_value(STD sym_table, int vid_offset, InitialValue i_value) {
-
-
-
+	if (!sym_table.plsBD)
+		return R_FAIL_1;
+	sym_table.pstvr[vid_offset].i_value = i_value;
+	return vid_offset;
 }
 
 /*******************************************************************************
@@ -103,9 +128,13 @@ Return Value:
 Algorithm:			
 *******************************************************************************/
 char st_get_type (STD sym_table, int vid_offset) {
-
-
-
+	if (sym_table.pstvr[vid_offset].status_field & STRINGFLAG == ISSTRING)
+		return 'S';
+	if (sym_table.pstvr[vid_offset].status_field & INTFLAG == ISINT)
+		return 'I';
+	if (sym_table.pstvr[vid_offset].status_field & FLOATFLAG == ISFLOAT)
+		return 'F';
+	return R_FAIL_1;
 }
 
 /*******************************************************************************
@@ -118,9 +147,13 @@ Return Value:
 Algorithm:			
 *******************************************************************************/
 void st_destroy(STD sym_table) {
-
-
-
+	int i = sym_table.st_size;
+	while(--i >= 0)
+	{
+		free(sym_table.pstvr[i].plex);
+	}
+	free(sym_table.pstvr);
+	b_destroy(sym_table.plsBD);
 }
 
 /*******************************************************************************
@@ -133,7 +166,7 @@ Return Value:
 Algorithm:			
 *******************************************************************************/
 int st_print(STD sym_table) {
-
+	sym_table.pstvr[0].o_line
 
 
 }
@@ -163,9 +196,7 @@ Return Value:
 Algorithm:			
 *******************************************************************************/
 static void st_incoffset(void) {
-
-
-
+	
 }
 
 /*******************************************************************************
@@ -195,3 +226,13 @@ Algorithm:
 int st_sort(STD sym_table, char s_order) {
 	return 0;
 }
+
+/*******************************************************************************
+Function Name:		ex
+Purpose:		Calculate t to the power of x
+In Parameters:		double, int
+Out Parameters:		double
+Version:		1.0
+Author:			Bryden Lacelle
+*******************************************************************************/
+int ex (int t, int x) { return (x > 0 ? t * ex(t, x-1) : 1); }
