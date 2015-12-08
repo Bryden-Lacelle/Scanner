5cca6f910f08ba883499ecb0546b07b31b9d1fd5
Bryden-Lacelle <lace0086@algonquinlive.com>
2015-12-08 13:04:51 -0500

    Update parser.h

--- parser.h
+++ parser.h
@@ -1,22 +1,46 @@
 /*****************************************************************************************************
- * File Name:		parser.h
- * Compiler:		MS Visual Studio 2012
- * Authors:			Bryden Lacelle & Justin Farinaccio
- * Course:			CST8152 - Compilers, Lab Section 011
- * Assignment:		Assignment 4: The Parser
- * Date:			December 11, 2015
- * Professor:		Svillen Ranev
- * Purpose:			Header file for parser.c; includes definitions for ___, as well as forward declartions for 
-					necessary functions.
- * Function List:	parser()
-					match()
-					syn_eh()
-					syn_printe()
-					gen_incode()
+* File Name:		parser.h
+* Compiler:		MS Visual Studio 2012
+* Authors:			Bryden Lacelle & Justin Farinaccio
+* Course:			CST8152 - Compilers, Lab Section 011
+* Assignment:		Assignment 4: The Parser
+* Date:			December 11, 2015
+* Professor:		Svillen Ranev
+* Purpose:			Header file for parser.c; includes definitions for ___, as well as forward declartions for
+necessary functions.
+* Function List:	parser()
+match()
+syn_eh()
+syn_printe()
+gen_incode()
 *****************************************************************************************************/
 
 #ifndef PARSER_H_
 #define PARSER_H_
+
+#ifndef TOKEN_H_
+#include "token.h"
+#endif
+
+#ifndef STABLE_H_
+#include "stable.h"
+#endif
+
+#ifndef BUFFER_H_
+#include "buffer.h"
+#endif
+
+extern char* kw_table[];
+/* definitions */
+#define NO_ATTR		0
+#define ELSE		0
+#define IF			1
+#define INPUT		2
+#define OUTPUT		3
+#define PLATYPUS	4
+#define REPEAT		5
+#define THEN		6
+#define USING		7
 
 /* parser functions */
 void parser(Buffer*);
@@ -29,18 +53,6 @@ void gen_incode(char*);
 extern Token mlwpar_next_token(Buffer*);
 extern int line;
 extern Buffer *str_LTBL;
-
-/* definitions */
-#define NO_ATTR		0
-
-#define ELSE		0
-#define IF			1
-#define INPUT		2
-#define OUTPUT		3
-#define PLATYPUS	4
-#define REPEAT		5
-#define THEN		6
-#define USING		7
 
 /* grammar productions */
 void program(void);
@@ -79,4 +91,4 @@ void primary_a_relational_expression_p(void);
 void primary_s_relational_expression(void);
 void primary_s_relational_expression_p(void);
 
-#endif	PARSER_H_
+#endif
